5266474b46034cf3b654b4bd8ac9d979a7f902c3
windy-cookies-web <7adrianj@gmail.com>
2026-09-19 14:48:21 -0300

    Delete components directory

--- components/order-provider.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from "react";
-import { OrderContext, useOrderState } from "../hooks/use-order";
-
-export function OrderProvider({ children }: { children: ReactNode }) {
-  const order = useOrderState();
-  return <OrderContext value={order}>{children}</OrderContext>;
-}
